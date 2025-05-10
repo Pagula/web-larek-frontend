9d159973c3d14bd2bc69d0b54871ca8e005038f7
Juriy Pagula <yurypagula@yandex.ru>
2025-05-10 18:12:20 +0500

    переменная для тригеров событий

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,27 @@ export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 export const settings = {
 
 };
+
+export const eventTriggers = {
+    //  корзина и товары //
+    itemChanged: 'items:changed',
+    basketChanged: 'basket:changed',
+    basketOpen: 'basket:open',
+    cardAdd: 'card:add',
+    cardRemove: 'card:remove',
+    // заказ и оформление //
+    orderSubmit:'order:submit',
+    orderOpen: 'order:open',
+    formErrorsOrder: 'formErrorsOrder:change',
+    orderChanged: /^order..*:change/,
+    contactsSubmit: 'contacts:submit',
+    formErrorsContact: 'formErrorsContact:change',
+    contactsChanged: /^contacts..*:change/,
+    paymentChange: 'payment:change',
+    // модальные окна //
+    modalOpen: 'modal:open',
+    modalClose: 'modal:close',
+    // предпросмотр, выбор товара //
+    cardSelect: 'card:select',
+    previewChanged: 'preview:changed',
+};
