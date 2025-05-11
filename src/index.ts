8a6b782edd168ea29bbdb62de7a28bbe0ac94f0c
Juriy Pagula <yurypagula@yandex.ru>
2025-05-11 10:20:21 +0500

    написал класс модального окна  его методы, и превью карточке

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { ProductAPI } from './components/ProductApi';
 import { Card } from './components/Card';
 import { Basket } from './components/common/Basket';
 import { AppState, CatalogChangeEvent } from './components/AppData';
+import { Modal } from './components/common/Modal';
 
 
 
@@ -28,6 +29,7 @@ const basketTemplate = ensureElement<HTMLTemplateElement>('#basket');
 const page = new Page(document.body,events);
 const basket = new Basket(cloneTemplate(basketTemplate), events);
 const card = new Card(cloneTemplate(cardCatalogTemplate));
+const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
 
 
 events.on<CatalogChangeEvent>(eventTriggers.itemChanged, () => {
@@ -41,6 +43,26 @@ events.on<CatalogChangeEvent>(eventTriggers.itemChanged, () => {
             price: item.price,
             category: item.category,
         });
+    });
+});
+
+events.on(eventTriggers.cardSelect, (item: IProductItem) => {
+    appData.setPreview(item);
+});
+
+events.on(eventTriggers.previewChanged, (item: IProductItem) => {
+    const card = new Card(cloneTemplate(cardPreviewTemplate), {
+        onClick: () => events.emit(eventTriggers.cardAdd, item),
+    });
+    card.isButtonDisabled(appData.order.items, item.id, item.price);
+    modal.render({
+        content: card.render({
+            title: item.title,
+            image: item.image,
+            price: item.price,
+            category: item.category,
+            description: item.description,
+        }),
     });
 });
 
